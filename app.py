3ac8f29443c9dd4530fb8b1f97834f716f94519b
Liam Tengelis <ltengelis@gmail.com>
2020-10-09 17:55:23 -0700

    styling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import dash
 import dash_core_components as dcc
+import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input
 from dash.dependencies import Output
@@ -11,7 +12,10 @@ import plotly.express as px
 import lorenz
 
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [
+    # 'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    "https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css",
+]
 
 
 axis_template = {
@@ -41,37 +45,43 @@ plot_layout = {
 
 def init() -> dash.Dash:
     app = dash.Dash(__name__,
-                    # external_stylesheets=external_stylesheets
+                    external_stylesheets=external_stylesheets
                     )
-    df = pd.DataFrame({
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples",
-                  "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-    })
     app.layout = html.Div(children=[
-        html.Div(children=[
-            html.Div(
-                u"\u03C1" + ": " + str("rho value..."),
-                style={"fontSize": "30px"}
-            ),
-            dcc.Slider(
-                id='rho-slider',
-                min=lorenz.RHO_MIN,
-                max=lorenz.RHO_MAX,
-                value=lorenz.rho,
-                step=lorenz.RHO_STEP,
-            ),
-            html.Div(
-                u"\u03B2" + ": " + str("beta value..."),
-                style={"fontSize": "30px"}
-            ),
-            dcc.Slider(
-                id='beta-slider',
-                min=lorenz.BETA_MIN,
-                max=lorenz.BETA_MAX,
-                value=lorenz.beta,
-                step=lorenz.BETA_STEP,
+        html.Div(className="flex", children=[
+            html.Div(className="w-20", children=[
+                html.Span("Show system 2"),
+                daq.ToggleSwitch(
+                    color="#add8e6",
+                    id='toggle-sys-2',
+                    value=True,
+                )
+            ]),
+            html.Div(className="w-40", children=[
+                html.Div(
+                    u"\u03C1" + ": " + str("rho value..."),
+                    style={"fontSize": "20px"}
+                ),
+                dcc.Slider(
+                    id='rho-slider',
+                    min=lorenz.RHO_MIN,
+                    max=lorenz.RHO_MAX,
+                    value=lorenz.rho,
+                    step=lorenz.RHO_STEP,
+                ),
+            ]),
+            html.Div(className="w-40", children=[
+                html.Div(
+                    u"\u03B2" + ": " + str("beta value..."),
+                    style={"fontSize": "20px"}
+                ),
+                dcc.Slider(
+                    id='beta-slider',
+                    min=lorenz.BETA_MIN,
+                    max=lorenz.BETA_MAX,
+                    value=lorenz.beta,
+                    step=lorenz.BETA_STEP,
+                )]
             ),
         ]),
         html.Div(id='my-output'),
@@ -88,42 +98,46 @@ server = app.server
 
 
 def gen_graph(
-        rho, beta
+        rho,
+        beta,
+        show_sys_2
 ):
-    df_1 = lorenz.simulate(
+    df = lorenz.simulate(
         rho=rho,
         beta=beta,
-        initial=[1.5, 1.0, 1.1]
+        initial=[1.0, 1.0, 100.0]
     )
-    df_2 = lorenz.simulate(
-        rho=rho,
-        beta=beta,
-        initial=[1.0, 1.0, 1.0]
-    )
-    df_1["initial"] = 1
-    df_2["initial"] = 2
+    df["initial"] = "1"
+    if show_sys_2:
+        df_2 = lorenz.simulate(
+            rho=rho,
+            beta=beta,
+            initial=[1.0, 1.0, 1.0]
+        )
+        df_2["initial"] = "2"
+        df = pd.concat((df, df_2))
     fig = px.scatter_3d(
-        pd.concat((df_1, df_2)),
+        df,
         x="x", y="y", z="z",
         color="initial",
         size="t",
-        size_max=15,
-        opacity=0.5,
+        size_max=20,
+        opacity=0.3,
     )
     fig["layout"] = plot_layout
+    fig.update_layout(transition_duration=500)
     return fig
 
 
 @app.callback(
     Output('graph', 'figure'),
     [Input(component_id="rho-slider", component_property="value"),
-     Input(component_id="beta-slider", component_property="value")]
+     Input(component_id="beta-slider", component_property="value"),
+     Input(component_id="toggle-sys-2", component_property="value"),]
 )
-def update_rho(rho, beta):
-    print(f"rho: {rho}, beta: {beta}")
-    fig = gen_graph(rho, beta)
-    fig.update_layout(transition_duration=500)
-    return fig
+def update_graph(rho, beta, show_sys_2):
+    print(f"rho: {rho}, beta: {beta}, toggle: {show_sys_2}")
+    return gen_graph(rho, beta, show_sys_2)
 
 
 if __name__ == '__main__':
